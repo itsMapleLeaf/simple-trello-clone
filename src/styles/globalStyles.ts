8d88a6fa2e58c49b8f74ae788ed74904c9033f61
kingdaro <kingdaro@gmail.com>
2017-12-03 21:06:41 -0500

    global style tweaks

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -6,15 +6,16 @@ export function applyGlobalStyles() {
       margin: 0;
       padding: 0;
       font-family: Roboto, sans-serif;
+      font-size: 18px;
     }
 
     h1, h2, h3, h4, h5, h6 {
+      font-family: 'Roboto Condensed', sans-serif;
+      font-weight: 400;
       margin: 0;
-      font-weight: 300;
     }
 
     h1 {
-      font-family: 'Roboto Condensed', sans-serif;
       font-weight: 300;
     }
 
