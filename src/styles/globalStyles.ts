0c5e1bdab5eea31608824ee702a6c7199c8414da
kingdaro <kingdaro@gmail.com>
2017-12-03 20:47:28 -0500

    tweak global styles for more reasonable defaults

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -9,14 +9,25 @@ export function applyGlobalStyles() {
     }
 
     h1, h2, h3, h4, h5, h6 {
+      margin: 0;
+      font-weight: 300;
+    }
+
+    h1 {
       font-family: 'Roboto Condensed', sans-serif;
       font-weight: 300;
-      margin: 0;
     }
 
     a {
       color: inherit;
       text-decoration: inherit;
+    }
+
+    ol, ul {
+      list-style: none;
+      padding-left: 0;
+      margin-top: 0;
+      margin-bottom: 0;
     }
   `
 }
