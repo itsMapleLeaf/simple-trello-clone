3437577aa4c3c0b4b3653a956db01b2d6c2a7043
kingdaro <kingdaro@gmail.com>
2017-12-03 20:36:00 -0500

    define global colors

---
+++ src/styles/colors.ts
@@ -0,0 +1,11 @@
+export const blue = '#3498db'
+export const blueDark = '#2980b9'
+
+export const green = '#2ecc71'
+export const greenDark = '#27ae60'
+
+export const asphalt = '#34495e'
+export const asphaltDark = '#2c3e50'
+
+export const clouds = '#ecf0f1'
+export const cloudsDark = '#bdc3c7'
